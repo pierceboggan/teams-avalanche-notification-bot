d1264ffc823e31d099a7f137bfc00b5141c71e6e
Pierce Boggan <piboggan@microsoft.com>
2022-03-29 19:00:19 -0600

    Added working notification-style bot

---
+++ bot/src/timerTrigger.ts
@@ -0,0 +1,40 @@
+import { AzureFunction, Context } from "@azure/functions";
+import { ConversationBot } from "@microsoft/teamsfx";
+import fetch from "node-fetch";
+import { buildAdaptiveCard } from "./adaptiveCard";
+import notificationTemplate from "./adaptiveCards/notification-default.json";
+
+// Time trigger to send notification. You can change the schedule in ../timerNotifyTrigger/function.json
+const timerTrigger: AzureFunction = async function (context: Context, myTimer: any): Promise<void> {
+
+  const timeStamp = new Date().toDateString();
+  const forecast = await getForecast();
+
+  for (const target of await ConversationBot.installations()) {
+    await target.sendAdaptiveCard(
+      buildAdaptiveCard(() => {
+        console.log(forecast);
+        return {
+          danger: `Forecast for ${timeStamp} is rated ${forecast.Danger}`,
+          rose: forecast.Rose
+        };
+      }, notificationTemplate)
+    );
+  }
+};
+
+function getForecast(): Promise<Forecast> {
+  var url = 'https://avalancheforecastapp.azurewebsites.net/api/AvalancheForecastFunction?code=imq/71/iVgkXnkbcZ9uAOi/0zWgdV5oOZ2NDWvIXjhG2w50djMttKg==';
+  return fetch(url)
+    .then(response => response.json())
+    .then(response => {
+      return response as Forecast
+    })
+}
+
+interface Forecast {
+  Danger: string
+  Rose: string
+}
+
+export default timerTrigger;
